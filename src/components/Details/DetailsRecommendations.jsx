bc8f1727fe2874fdc7300517dcd19f55750b6f81
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-09-13 15:19:51 -0300

    shuffled recommendations and added redirect on click

--- src/components/Details/DetailsRecommendations.jsx
+++ src/components/Details/DetailsRecommendations.jsx
@@ -5,21 +5,21 @@ import RecommendationCard from './RecommendationCard';
 
 const six = 6;
 
-// const shuffle = (array) => {
-//   const arr = [...array];
-//   for (let i = arr.length - 1; i > 0; i -= 1) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [arr[i], arr[j]] = [arr[j], arr[i]];
-//   }
-//   return arr;
-// };
+const shuffle = (array) => {
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
 
 function DetailsRecommendations({ spec }) {
   const recipesList = useSelector((state) => state.api.recipesList);
 
   if (!recipesList) return null;
 
-  // const shuffledList = shuffle(recipesList).slice(0, six);
+  const shuffledList = shuffle(recipesList).slice(0, six);
   const id = spec === 'Meal' ? 'idDrink' : 'idMeal';
 
   return (
@@ -28,7 +28,7 @@ function DetailsRecommendations({ spec }) {
       <div className="recomendation-cards-outercontainer">
         <div className="recomendation-cards-wrapper">
           <div className="recomendation-cards-innercontainer">
-            {recipesList.slice(0, six).map((r, i) => (
+            {shuffledList.map((r, i) => (
               <RecommendationCard key={ r[id] } rec={ r } idx={ i } spec={ spec } />
             ))}
           </div>
